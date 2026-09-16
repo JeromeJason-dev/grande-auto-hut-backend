f4b2e14c5e887dfe9b2519766f9b9a84030563a9
JeromeJason <jasonnem048@gmail.com>
2026-09-16 11:23:55 +0300

    refactor: adjust for login to generate token

--- auto_project/urls.py
+++ auto_project/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
 
     # Auth
     path("api/auth/", include("accounts.urls")),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Feature apps - fitment MUST come before catalog: catalog's
